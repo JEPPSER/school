b2156716259100213d021cdfeae3bc34bd8cc355
Jesper Bergström <jesper.b96@gmail.com>
2020-02-01 10:42:44 +0100

    Finishes substitution and transposition encyption and decryption methods

--- 1DV700/Assignment1/src/main/Main.java
+++ 1DV700/Assignment1/src/main/Main.java
@@ -1,13 +1,21 @@
 package main;
 
-import main.encryption.Substitution;
+import main.encryption.Encryption;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Substitution s = new Substitution();
-		String encrypted = s.encrypt("hello world! It is very nice to be here. Thanks for having me!", (char) 65);
+		
+		// substitution
+		String encrypted = Encryption.subEncrypt("hello world! It is very nice to be here. Thanks for having me!", (char) 65);
 		System.out.println(encrypted);
-		System.out.println(s.decrypt(encrypted, (char) 65));
+		System.out.println(Encryption.subDecrypt(encrypted, (char) 65));
+		
+		System.out.println();
+		
+		// transposition
+		encrypted = Encryption.transEncrypt("We are discovered flea at once!! haha aa de ere", "zebra");
+		System.out.println(encrypted);
+		System.out.println(Encryption.transDecrypt(encrypted, "zebra"));
 	}
 }
--- 1DV700/Assignment1/src/main/encryption/Encryption.java
+++ 1DV700/Assignment1/src/main/encryption/Encryption.java
@@ -1,6 +1,97 @@
 package main.encryption;
 
-public interface Encryption {
-	public String encrypt(String message, char key);
-	public String decrypt(String message, char key);
+import java.util.Arrays;
+
+public class Encryption {
+	
+	public static String subEncrypt(String message, char key) {
+		String result = "";
+		for (byte b : message.getBytes()) {
+			result += (char) ((int) b ^ (int) key);
+		}
+		return result;
+	}
+	
+	public static String subDecrypt(String message, char key) {
+		String result = "";
+		for (byte b : message.getBytes()) {
+			result += (char) ((int) b ^ (int) key);
+		}
+		return result;
+	}
+	
+	public static String transEncrypt(String message, String key) {
+		// Derive encryption order from key.
+		char[] chars = key.toCharArray();
+		Arrays.sort(chars);
+		int[] positions = new int[chars.length];
+		for (int i = 0; i < chars.length; i++) {
+			positions[i] = indexOfChar(chars, key.charAt(i));
+		}
+		
+		// Create encryption matrix.
+		char[][] matrix = new char[(int) Math.ceil((float) message.length() / (float) key.length())][key.length()];
+		
+		// Fills matrix with chars from message.
+		int counter = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (counter >= message.length()) {
+					break;
+				}
+				matrix[i][j] = message.charAt(counter);
+				counter++;
+			}
+		}
+		
+		// Creates a string from reading the columns in the matrix in the order derived from the key.
+		String result = "";
+		for (int i : positions) {
+			for (int j = 0; j < matrix.length; j++) {
+				result += matrix[j][i];
+			}
+		}
+		return result;
+	}
+	
+	public static String transDecrypt(String message, String key) {
+		// Derive encryption order of columns from key.
+		char[] chars = key.toCharArray();
+		Arrays.sort(chars);
+		int[] positions = new int[chars.length];
+		for (int i = 0; i < chars.length; i++) {
+			positions[i] = indexOfChar(chars, key.charAt(i));
+		}
+		
+		// Create encryption matrix.
+		char[][] matrix = new char[(int) Math.ceil((float) message.length() / (float) key.length())][key.length()];
+		
+		// Fill the matrix with chars from message in order derived from key.
+		int counter = 0;
+		for (int i : positions) {
+			for (int j = 0; j < matrix.length; j++) {
+				matrix[j][i] = message.charAt(counter);
+				counter++;
+			}
+		}
+		
+		// Read the matrix in normal order and put in result string.
+		String result = "";
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				result += matrix[i][j];
+			}
+		}
+		
+		return result;
+	}
+	
+	private static int indexOfChar(char[] chars, char c) {
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i] == c) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
