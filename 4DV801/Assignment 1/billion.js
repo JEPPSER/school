dee7e62bd1252249dd54fd27d16d6c3d3e5adb7c
Jesper Bergström <jesper.b96@gmail.com>
2020-04-03 15:12:22 +0200

    Implements "zooming" in treemap

--- 4DV801/Assignment 1/billion.js
+++ 4DV801/Assignment 1/billion.js
@@ -5,37 +5,51 @@ var margin = { top: 10, right: 10, bottom: 10, left: 10 },
     width = 1200 - margin.left - margin.right,
     height = 900 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-var svg = d3.select("#billion")
+d3.json("https://raw.githubusercontent.com/JEPPSER/school/master/4DV801/Assignment%201/BillionDollars.json", function (data) {
+    switchData(data, null);
+})
+
+function switchData(data, color) {
+    d3.select("svg").remove();
+
+    var svg = d3.select("#billion")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-d3.json("https://raw.githubusercontent.com/JEPPSER/school/master/4DV801/Assignment%201/BillionDollars.json", function (data) {
-
-    var keys = ["earning", "losing", "fighting", "spending", "hustling", "giving"];
-    var color = d3.scaleOrdinal()
-        .domain(keys)
-        .range(["#402D54", "#b86b51", "#38ab9a", "#088f1a", "#8F0000", "#2284a8"])
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Calculate the size of the high level children.
-    for (var i = 0; i < data.children.length; i++) {
-        var sum = 0;
-        for (var j = 0; j < data.children[i].children.length; j++) {
-            sum += data.children[i].children[j].size;
+    if (data.name == "World wide spending in billion dollars") {
+        for (var i = 0; i < data.children.length; i++) {
+            var sum = 0;
+            for (var j = 0; j < data.children[i].children.length; j++) {
+                sum += data.children[i].children[j].size;
+            }
+            data.children[i].size = sum;
         }
-        data.children[i].size = sum;
     }
 
-    // Set colors
-    for (var i = 0; i < data.children.length; i++) {
-        var c = color(data.children[i].name);
-        setColor(data.children[i], c);
+    if (color == null) {
+        var keys = ["earning", "losing", "fighting", "spending", "hustling", "giving"];
+        var color = d3.scaleOrdinal()
+            .domain(keys)
+            .range(["#402D54", "#b86b51", "#38ab9a", "#088f1a", "#8F0000", "#2284a8"])
+        
+        // Set colors
+        if (data.children != undefined) {
+            for (var i = 0; i < data.children.length; i++) {
+                var c = color(data.children[i].name);
+                setColor(data.children[i], c);
+            }
+        }
+        data.color = "#FFF";
+    } else if (data.children != undefined) {
+        for (var i = 0; i < data.children.length; i++) {
+            setColor(data.children[i], color);
+        }
+        data.color = color;
     }
-    data.color = "#FFFFFF";
 
     var treemapLayout = d3.treemap()
         .size([width, height])
@@ -55,7 +69,10 @@ d3.json("https://raw.githubusercontent.com/JEPPSER/school/master/4DV801/Assignme
         .attr("width", function(d) { return d.x1 - d.x0 })
         .attr("height", function(d) { return d.y1 - d.y0 })
         .attr("fill", function(d) { return d.data.color })
-})
+        .on("click", function (d) {
+            switchData(d.data, d.data.color);
+        })
+}
 
 function setColor(node, c) {
     node.color = c;
