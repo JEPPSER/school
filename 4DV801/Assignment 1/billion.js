f49b32c33637758fe0862bafa2cfe1a8c10cf1b3
Jesper Bergström <jesper.b96@gmail.com>
2020-04-04 15:00:58 +0200

    Adds detail on demand and labels to treemap

--- 4DV801/Assignment 1/billion.js
+++ 4DV801/Assignment 1/billion.js
@@ -1,11 +1,20 @@
 var info = document.getElementById("info");
 
+var svgElem = document.getElementById("billion");
+svgElem.addEventListener("contextmenu", function () {
+    event.preventDefault();
+    return false;
+});
+
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 10, bottom: 10, left: 10 },
-    width = 1200 - margin.left - margin.right,
-    height = 900 - margin.top - margin.bottom;
+    width = 1000 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+
+var original;
 
 d3.json("https://raw.githubusercontent.com/JEPPSER/school/master/4DV801/Assignment%201/BillionDollars.json", function (data) {
+    original = data;
     switchData(data, null);
 })
 
@@ -13,11 +22,11 @@ function switchData(data, color) {
     d3.select("svg").remove();
 
     var svg = d3.select("#billion")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
     // Calculate the size of the high level children.
     if (data.name == "World wide spending in billion dollars") {
@@ -35,7 +44,7 @@ function switchData(data, color) {
         var color = d3.scaleOrdinal()
             .domain(keys)
             .range(["#402D54", "#b86b51", "#38ab9a", "#088f1a", "#8F0000", "#2284a8"])
-        
+
         // Set colors
         if (data.children != undefined) {
             for (var i = 0; i < data.children.length; i++) {
@@ -53,10 +62,9 @@ function switchData(data, color) {
 
     var treemapLayout = d3.treemap()
         .size([width, height])
-        .paddingOuter(10);
+        .paddingOuter(25);
 
     var root = d3.hierarchy(data).sum(function (d) { return d.size });
-    
     treemapLayout(root);
 
     svg
@@ -64,13 +72,42 @@ function switchData(data, color) {
         .data(root.descendants())
         .enter()
         .append("rect")
-        .attr("x", function(d) { return d.x0; })
-        .attr("y", function(d) { return d.y0; })
-        .attr("width", function(d) { return d.x1 - d.x0 })
-        .attr("height", function(d) { return d.y1 - d.y0 })
-        .attr("fill", function(d) { return d.data.color })
+        .attr("x", function (d) { return d.x0; })
+        .attr("y", function (d) { return d.y0; })
+        .attr("width", function (d) { return d.x1 - d.x0 })
+        .attr("height", function (d) { return d.y1 - d.y0 })
+        .attr("fill", function (d) { return d.data.color })
         .on("click", function (d) {
             switchData(d.data, d.data.color);
+        })
+        .on("contextmenu", function (d) {
+            switchData(original, null);
+        })
+        .on("mouseover", function (d) {
+            info.innerHTML = "<h1>" + d.data.name + ": " + d.value;
+        })
+        .on("mouseout", function (d) {
+            info.innerHTML = "";
+        })
+
+    var nodes = d3.select('svg g')
+        .selectAll('g')
+        .data(root.descendants())
+        .enter()
+        .append('g')
+        .attr('transform', function (d) { return 'translate(' + [d.x0, d.y0] + ')' })
+
+    nodes
+        .append('text')
+        .attr('dx', 4)
+        .attr('dy', 16)
+        .text(function (d) {
+            var width = d.x1 - d.x0;
+            var height = d.y1 - d.y0;
+            if (d.data.name.length > width / 8 || height < 12) {
+               return ""; 
+            }
+            return d.data.name;
         })
 }
 
