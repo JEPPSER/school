8ce871efbb366566fcbcf48e03197a686662a3b6
Jesper Bergström <jesper.b96@gmail.com>
2020-04-28 15:39:22 +0200

    Adds the map image as background in scene

--- 4DV801/Assignment3/mainwindow.cpp
+++ 4DV801/Assignment3/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
 {
     Q_UNUSED(parent);
 
-    Scene = new MapScene(0, 0, 500, 500);
-    Scene->setBackgroundBrush(QColor(0, 0, 0));
+    Scene = new MapScene(0, 0, 700, 700);
+    QPixmap pim = QPixmap(":/images/mock.png");
+    qreal scale = Scene->width() / pim.width();
+    Scene->addPixmap(pim.scaled(Scene->width(), pim.height() * scale));
 
     View = new MapView(Scene);
     View->setMouseTracking(true);
